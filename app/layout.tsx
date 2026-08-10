e0ee5a545ce704b7c72f58dc71a490f8d8cca533
Ravi Shankar Teja <yravishankarteja@gmail.com>
2026-08-10 15:45:43 +0530

    Added Tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,29 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        {/* Google-provided snippet is kept verbatim for the requested head placement. */}
+        {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NTH8PNQW');`,
+          }}
+        />
+      </head>
       <body className="min-h-full bg-[#050806]">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NTH8PNQW"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <SiteHeaderClient />
         <AnimeSiteMotion />
         {children}
